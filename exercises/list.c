9c09e0823235eca457ddcdd7d0329c6377543fcd
Richard Pappalardo <rpappalax@gmail.com>
2016-09-24 17:10:11 -0700

    chore: cleanup exercises, add run file

--- exercises/list.c
+++ exercises/list.c
@@ -16,19 +16,6 @@ void print_log_header(char *);
 ***************************************/
 int main();
 
-void list_init(List *list, void (*destroy)(void *data));
-void list_destroy(List *list);
-//int list_ins_next(List *list, ListElmt *element, const void *data);
-int list_rem_next(List *list, ListElmt *element, void **data);
-
-#define list_size(list) ((list)->size)
-#define list_head(list) ((list)->head)
-#define list_tail(list) ((list)->tail)
-#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
-#define list_is_tail(element) ((element)->next == NULL ? 1 : 0)
-#define list_data(element) ((element)->data)
-#define list_next(element) ((element)->next)
-
 
 /*
  * Description: Illustrates using a linked list (see Chapter 5).
@@ -247,6 +234,8 @@ void list_destroy(List *list) {
 
 /* ----------------------------- list_ins_next --------------------------- */
 
+// SOURCE IN: list_ins_next.c
+
 /* ----------------------------- list_rem_next --------------------------- */
 int list_rem_next(List *list, ListElmt *element, void **data) {
 
@@ -291,4 +280,5 @@ int list_rem_next(List *list, ListElmt *element, void **data) {
     return 0;
 
 }
+
 
