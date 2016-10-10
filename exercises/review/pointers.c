1e0165c2ee5a6db5087fdf59954d2f6b840d78c0
Richard Pappalardo <rpappalax@gmail.com>
2016-10-09 22:42:09 -0700

    feat: add basic pointer exercise

--- exercises/review/pointers.c
+++ exercises/review/pointers.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "utils.h"
 
 
 // SYNTAX
@@ -18,18 +19,34 @@ const int *pci;
 // 3. pci is a pointer variable to an integer 
 // 4. pci is a pointer variable to a constant integer 
 
+void basic();
+
 void basic() {
     int num = 77;
     int *num_ptr = &num;
 
+    //                          +----------------+
+    // num 77    0x7fff59191824 |             77 |
+    //                          +----------------+
+    // num_ptr   Ox7fff5919182c | 0x7fff59191824 |
+    //                          +----------------+
+    //           0x7fff59191834 |                |
+    //                          +----------------+
+
+
+
+    print_log_header("VAR VALUES"); 
     printf("num: %d\n", num);
-    printf("the address of the number....\n");
-    printf("&num: %p\n", &num);
     printf("num_ptr: %p\n", num_ptr);
-    printf("*num_ptr: %i\n", *num_ptr);
+    printf("*num_ptr: %i\n\n", *num_ptr);
+
+    print_log_header("VAR ADDRESSES"); 
+    printf("&num: %p\n", &num);
+    printf("&num_ptr: %p\n\n\n", &num_ptr);
 }
 
 int main() {
+    print_log_header_dbl("POINTER DEMO"); 
     basic();
     return 0;
 }
