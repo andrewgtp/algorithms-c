7074f613e8e0c4b65561688c7252ffc713cc35f6
Richard Pappalardo <rpappalax@gmail.com>
2016-12-05 16:18:42 -0800

    fix: tweak run file

--- exercises/run.sh
+++ exercises/run.sh
@@ -15,7 +15,7 @@ echo "or run the ANSWER code."
 echo 
 echo "Use the diagrams as a hint."
 echo
-options=("ANSWER-code" "QUESTION-code" "Quit")
+options=("ANSWER-code" "list_ins_next_QUESTION" "list_rem_next_QUESTION" "Quit")
 PS3="Select the option above you want to compile/run:  "
 
 select opt in "${options[@]}"
@@ -26,7 +26,12 @@ do
             $CC list.out list.c list_ins_next_ANSWER.c list_rem_next_ANSWER.c
             break
             ;;
-       "QUESTION-code")
+       "list_ins_next_QUESTION")
+            echo "RUN: your QUESTION code." 
+            $CC list.out list.c list_ins_next_QUESTION.c list_rem_next_ANSWER.c
+            break
+            ;;
+       "list_rem_next_QUESTION")
             echo "RUN: your QUESTION code." 
             $CC list.out list.c list_ins_next_ANSWER.c list_rem_next_QUESTION.c
             break
