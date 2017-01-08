3b77bd0dd92529ca8a9c657e2f7a8cfb868a2fe6
Richard Pappalardo <rpappalax@gmail.com>
2017-01-08 10:46:58 -0800

    chore: setup run file and headers for dlist

--- exercises/list.c
+++ exercises/list.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-#include "headers.h"
+#include "list.h"
 
 
 /***************************************
--- exercises/run.sh
+++ exercises/run.sh
@@ -16,13 +16,13 @@ echo
 echo "Use the diagrams as a hint."
 echo
 
-options=("ANSWER-code" "list_ins_next_QUESTION" "list_rem_next_QUESTION" "Quit")
+options=("ANSWER-code-list" "list_ins_next_QUESTION" "list_rem_next_QUESTION" "ANSWER-code-dlist" "dlist_ins_next_QUESTION" "dlist_rem_next_QUESTION" "Quit")
 PS3="Select the option above you want to compile/run.  Make sure you create your own QUESTION file first:"
 
 select opt in "${options[@]}"
 do
     case $opt in
-       "ANSWER-code")
+       "ANSWER-code-list")
             echo "RUN: list_ins_next_ANSWER (same as above, but includes diagrams)"
             $CC list.out list.c list_ins_next_ANSWER.c list_rem_next_ANSWER.c
             break
@@ -35,6 +35,21 @@ do
        "list_rem_next_QUESTION")
             echo "RUN: your QUESTION code." 
             $CC list.out list.c list_ins_next_ANSWER.c list_rem_next_QUESTION.c
+            break
+            ;;
+       "ANSWER-code-dlist")
+            echo "RUN: dlist_ins_next_ANSWER (same as above, but includes diagrams)"
+            $CC dlist.out dlist.c dlist_ins_next_ANSWER.c dlist_rem_next_ANSWER.c
+            break
+            ;;
+       "dlist_ins_next_QUESTION")
+            echo "RUN: your QUESTION code." 
+            $CC dlist.out dlist.c dlist_ins_next_QUESTION.c dlist_rem_next_ANSWER.c
+            break
+            ;;
+       "dlist_rem_next_QUESTION")
+            echo "RUN: your QUESTION code." 
+            $CC dlist.out dlist.c dlist_ins_next_ANSWER.c dlist_rem_next_QUESTION.c
             break
             ;;
         "Quit")
